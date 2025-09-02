94269c227835207fcbb3926e22bcb8ae95149579
Nick S. <astrobouldergx@gmail.com>
2025-09-02 18:49:33 -0500

    Started work on Version 4 as mentioned in the notes.

--- Class1.java
+++ Class1.java
@@ -16,4 +16,8 @@ public class Class1 {
     private void f3(){
         //v3.0
     }
+    private void f4(){
+        //v4.0
+        //I realized that Step 8 tells us exactly how much you plan to look at it.
+    }
 }
